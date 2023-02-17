b76ce83720583817a3b74d6cd68107997224a2c0
UnesseAh <youness.ahasla@gmail.com>
2023-02-17 15:06:12 +0100

    create the tables with migration

--- database/migrations/2023_02_17_114125_add_roleid_to_users_table.php
+++ database/migrations/2023_02_17_114125_add_roleid_to_users_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
 
         });
     }
@@ -32,6 +27,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_id');
+        });
     }
 };
