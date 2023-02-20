e9f895402f2de1b00e9839c3d7cd593dc8e096d8
Salah <salaheddinehanaoui@gmail.com>
2023-02-20 15:23:57 +0100

    RACT-11-Finished making tags crud

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,16 @@ use App\Http\Controllers\CategoryController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+// Categories
 Route::get('categories', [CategoryController::class, 'index']);
 Route::post('categories', [CategoryController::class, 'store']);
 Route::get('categories/{category}', [CategoryController::class, 'show']);
 Route::put('categories/{category}', [CategoryController::class, 'update']);
 Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+
+// Tags
+Route::get('tags', [TagController::class, 'index']);
+Route::post('tags', [TagController::class, 'store']);
+Route::get('tags/{tag}', [TagController::class, 'show']);
+Route::put('tags/{tag}', [TagController::class, 'update']);
+Route::delete('tags/{tag}', [TagController::class, 'destroy']);
