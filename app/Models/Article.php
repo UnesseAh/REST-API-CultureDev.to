826841e784b87c2e474dde6bdb16903dc9264a2e
HIND-HANSALI <hindhsl111@gmail.com>
2023-02-22 09:38:59 +0100

    add article model with relations

---
+++ app/Models/Article.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'content',
+        'category_id',
+        'tag_id',
+        'user_id',
+    ];
+
+    // public function categorie()
+    // {
+    //     return $this->belongsTo(Category::class);
+    // }
+    // public function tags(){
+    //     return $this->belongsToMany(Tag::class);
+    // }
+    // public function user(){
+    //     return $this->hasOne(User::class);
+    // }
+    // public function comments(){
+    //     return $this->hasMany(Comment::class);
+    // }
+}
